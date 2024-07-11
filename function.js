6bebc561ad26ba1e22438cb268db859cd11a9238
SP Acharya <sthitaprangya.acharya@collegedekho.com>
2024-07-11 18:26:15 +0530

    intership day4

---
+++ function.js
@@ -0,0 +1,42 @@
+// function demo(){
+//     return "Good Evening";
+// }
+// console.log(demo());
+
+// const demo = function(){
+//     console.log("Good Evening");
+// }
+// demo()
+
+
+// Arrow function
+// const demo=(text)=>{
+//    return text
+// }
+// console.log(demo(5));
+
+// Annonomous function
+// which doesnot have any name
+// function(){
+
+// }
+
+// IIFE or self invoking function
+
+// (()=>{var a=10;
+//     console.log(a)
+// })()
+
+// let numbers = [32,12,23,15,11,20,18];
+// let doubled = numbers.reduce((acc,cur)=>acc+cur)
+// acc= index 0
+
+// acc = acc+cur
+// acc = 
+// console.log(doubled)
+
+
+
+
+
+
